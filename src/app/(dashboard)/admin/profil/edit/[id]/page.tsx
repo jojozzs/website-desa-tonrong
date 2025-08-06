3af3fb254496ecf1f174eb6bc63bceb30662f479
adhimrahman <adhimrahaman@gmail.com>
2025-08-06 23:18:34 +0800

    feat(profil): add edit, list, and add pages

---
+++ src/app/(dashboard)/admin/profil/edit/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilEdit() {
+
+    return (
+        <div>
+            <h1>Profil Edit</h1>
+        </div>
+    )
+}
---
+++ src/app/(dashboard)/admin/profil/tambah/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilTambah() {
+
+    return (
+        <div>
+            <h1>Profil Tambah</h1>
+        </div>
+    )
+}
