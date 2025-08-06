bf9f40bfc83cd51c48ed9980ec51db6c10f1f82d
adhimrahman <adhimrahaman@gmail.com>
2025-08-06 23:17:35 +0800

    feat(berita-pengumuman): add edit and add pages

---
+++ src/app/(dashboard)/admin/berita-dan-pengumuman/edit/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function BeritaPengumumanEdit() {
+
+    return (
+        <div>
+            <h1>Berita Pengumuman Edit</h1>
+        </div>
+    )
+}
---
+++ src/app/(dashboard)/admin/berita-dan-pengumuman/page.tsx
@@ -0,0 +1,8 @@
+export default function BeritaPengumumanList() {
+
+    return (
+        <div>
+            <h1>Berita Pengumuman List</h1>
+        </div>
+    )
+}
